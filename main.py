dc74a992e28c212b1f803d7cdbe2496bb63e9fb1
Pipotheavenger <sebastianfelipevalencia@hotmail.com>
2024-01-04 02:13:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
-import time
-import os
 from PIL import Image
 from PIL.ExifTags import TAGS
+from streamlit_extras.switch_page_button import switch_page
 import arrow #Para obtener la fecha y hora actual
 
 #Estructura del web app
+st.set_page_config(page_title="Formulario",initial_sidebar_state="collapsed")
 url_hoja = "https://docs.google.com/spreadsheets/d/1C6VKhqZUP7b8NNubpG3Il6nM7M18lTqkHKyAVvBwne4/edit?usp=sharing"
-st.title("Consignio de Horas extra Servicios")
-logo = st.image("https://media.discordapp.net/attachments/1079829553146503269/1190861752729088110/logo.png?ex=65a3576e&is=6590e26e&hm=34bf4073cb4d2fb670e2cbc314e92b713d51b38f297d8d2224c8b550f7da3232&=&format=webp&quality=lossless&width=981&height=497", width=200)
-st.markdown("Ingrese los siguientes datos para validar las Horas extras trabajadas")
-
 #Establecer conexion con google sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
 
@@ -22,8 +18,12 @@ existing_data = existing_data.dropna(how="all")
 
 #st.dataframe(existing_data)
 
-#Creacion del formulario
+#Creacion del formularioe 
 
+
+st.title("Consignio de Horas extra Servicios")
+logo = st.image("https://media.discordapp.net/attachments/1079829553146503269/1190861752729088110/logo.png?ex=65a3576e&is=6590e26e&hm=34bf4073cb4d2fb670e2cbc314e92b713d51b38f297d8d2224c8b550f7da3232&=&format=webp&quality=lossless&width=981&height=497", width=200)
+st.markdown("Ingrese los siguientes datos para validar las Horas extras trabajadas")
 Nombre = st.text_input(label="Ingrese su nombre")
 Pedido = st.text_input(label="Ingrese el Código de pedido")
 Cliente = st.text_input(label="Ingrese el nombre del cliente")
@@ -39,7 +39,7 @@ if imgfield == "Tomar Foto":
     file = st.camera_input(label="cargue su imagen")
     if file is not None:
         fecha = arrow.now('US/Eastern').format('YYYY-MM-DD HH:mm')
-        st.success("la foto ha sido cargada "+str(fecha))
+        st.success("la foto ha sido cargada ")
         #st.image(file, caption="Imagen desde la cámara", use_column_width=True)
 
 elif imgfield == "Cargar Imagen":
@@ -51,7 +51,7 @@ elif imgfield == "Cargar Imagen":
             if tag in TAGS:
                 exif[TAGS[tag]] = value
         fecha = exif['DateTimeOriginal']
-        st.success("la foto ha sido cargada con fecha "+str(fecha))
+        st.success("la foto ha sido cargada")
 
         #st.image(file, caption="Imagen desde el archivo", use_column_width=True)
 elif imgfield == "Seleccionar":
@@ -86,5 +86,4 @@ if Submit:
 
         updated_df = pd.concat([existing_data,new_data],ignore_index=True)
         conn.update(worksheet="Datos",data=updated_df)
-        st.success("Los datos han sido cargados exitosamente")
-
+        switch_page("final")
